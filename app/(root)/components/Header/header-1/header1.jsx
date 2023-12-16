faf3c6d52c74c6122d98db5e4361fe8892a2bd90
Mohammad-abdo <sdm800884@gmail.com>
2023-12-16 17:40:59 +0200

    first commit

---
+++ app/(root)/components/Header/header-1/header1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const header1 = () => {
+  return (
+    <div>
+      <div className="container my-3">
+        <div className="flex align-items-center justify-content-between">
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default header1
---
+++ app/(root)/components/Header/header-3/header-3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header3 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default header3
